c1ca36ae052c79a9d5e531e8c5a784a368e05212
Biplob Hossain <biplob17h@gmail.com>
2024-03-23 00:11:50 -0700

    update

--- src/App.jsx
+++ src/App.jsx
@@ -6,10 +6,13 @@ import Navbar from "./components/Navbar/Navbar";
 import Footer from "./components/Footer/Footer";
 import About from "./pages/About/About";
 import Session from "./pages/Session/Session";
+import { Toaster } from "react-hot-toast";
+import Profile from "./pages/Profile/Profile";
 
 const App = () => {
   return (
     <div>
+      <Toaster position="top-center" reverseOrder={false} />
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
@@ -17,6 +20,7 @@ const App = () => {
         <Route path="/login" element={<Login />} />
         <Route path="/about" element={<About />} />
         <Route path="/session" element={<Session />} />
+        <Route path="/profile" element={<Profile />} />
       </Routes>
       <Footer />
     </div>
